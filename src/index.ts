54fa87cdd77617fc32bc66f90b21fa43d0b6f88c
gerardo <gerardoramirez656@gmail.com>
2023-06-14 09:35:42 -0300

    test library

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+/**
+ * add two numbers 
+ * @param a number
+ * @param b number
+ * @returns result
+ */
+
+export const add =(a:number,b:number):number=>a + b;
+/**
+ * substract two numbers 
+ * @param a number
+ * @param b number
+ * @returns result
+ */
+
+export const substract =(a:number,b:number):number=>a - b;
+/**
+ * multiply two numbers 
+ * @param a number
+ * @param b number
+ * @returns result
+ */
+
+export const multiply=(a:number,b:number):number=>a * b;
+
